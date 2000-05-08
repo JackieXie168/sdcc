7d6c6a6f796d5657b029eddc507f2f57074ca26c
nobody <nobody@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2000-05-08 03:12:19 +0000

    This commit was manufactured by cvs2svn to create branch 'gbdk-294'.

---
+++ branches/gbdk-294/sdcc/src/avr/main.c
@@ -0,0 +1,196 @@
+/** @file main.c
+    avr specific general functions.
+
+    Note that mlh prepended _avr_ on the static functions.  Makes
+    it easier to set a breakpoint using the debugger.
+*/
+#include "common.h"
+#include "main.h"
+#include "ralloc.h"
+#include "gen.h"
+
+static char _defaultRules[] =
+{
+#include "peeph.rul"
+};
+
+/* list of key words used by msc51 */
+static char *_avr_keywords[] =     {
+    "at",
+    "code",
+    "critical",
+    "eeprom",
+    "interrupt",
+    "sfr",
+    "sbit",
+    "xdata",
+    "_code",
+    "_eeprom",
+    "_generic",
+    "_xdata",
+    "sram" ,
+    "_sram",
+    "flash",
+    "_flash",
+    NULL
+};
+
+static int regParmFlg = 0; /* determine if we can register a parameter */
+
+static void _avr_init(void)
+{
+    asm_addTree(&asm_asxxxx_mapping);
+}
+
+static void _avr_reset_regparm()
+{
+    regParmFlg = 0;
+}
+
+static int _avr_regparm( link *l)
+{
+    /* the first eight bytes will be passed in
+       registers r16-r23. but we won't split variables
+       i.e. if not enough registers left to hold
+       the parameter then the whole parameter along
+       with rest of the parameters go onto the stack */
+    if (regParmFlg < 8 ) {
+	int size ;
+	if ((size = getSize(l)) > (8 - regParmFlg)) {
+	    /* all remaining go on stack */
+	    regParmFlg = 8;
+	    return 0;
+	}
+	regParmFlg += size;
+	return 1;
+    }
+    
+    return 0;
+}
+
+void avr_assignRegisters (eBBlock **ebbs, int count);
+
+static bool _avr_parseOptions(int *pargc, char **argv, int *i)
+{
+    /* TODO: allow port-specific command line options to specify
+     * segment names here.
+     */
+    return FALSE;
+}
+
+static void _avr_finaliseOptions(void)
+{
+    port->mem.default_local_map =
+	port->mem.default_globl_map = xdata;
+    /* change stack to be in far space */
+    /* internal stack segment ;   
+       SFRSPACE       -   NO
+       FAR-SPACE      -   YES
+       PAGED          -   NO
+       DIRECT-ACCESS  -   NO
+       BIT-ACCESS     -   NO
+       CODE-ACESS     -   NO 
+       DEBUG-NAME     -   'B'
+       POINTER-TYPE   -   POINTER
+    */
+    istack	  = allocMap (0, 1, 0, 0, 0, 0,options.stack_loc, ISTACK_NAME,'B',FPOINTER);
+
+}
+
+static void _avr_setDefaultOptions(void)
+{
+    options.stackAuto = 1;
+}
+
+static const char *_avr_getRegName(struct regs *reg)
+{
+    if (reg)
+	return reg->name;
+    return "err";
+}
+
+static void _avr_genAssemblerPreamble(FILE *of)
+{
+
+}
+
+/* Generate interrupt vector table. */
+static int _avr_genIVT(FILE *of, symbol **interrupts, int maxInterrupts)
+{
+    return TRUE;
+}
+
+/** $1 is always the basename.
+    $2 is always the output file.
+    $3 varies
+    $l is the list of extra options that should be there somewhere...
+    MUST be terminated with a NULL.
+*/
+static const char *_linkCmd[] = {
+    "aslink", "-nf", "$1", NULL
+};
+
+static const char *_asmCmd[] = {
+    "asx8051", "-plosgffc", "$1.asm", NULL
+};
+
+/* Globals */
+PORT avr_port = {
+    "avr",
+    "ATMEL AVR",		/* Target name */
+    {
+	TRUE,			/* Emit glue around main */
+    },
+    {	
+	_asmCmd,
+	"-plosgffc",		/* Options with debug */
+	"-plosgff",		/* Options without debug */
+    },
+    {
+	_linkCmd
+    },
+    {
+	_defaultRules
+    },
+    {
+    	/* Sizes: char, short, int, long, ptr, fptr, gptr, bit, float, max */
+	1, 1, 2, 4, 2, 2, 3, 1, 4, 4
+    },
+    {
+	"XSEG    (XDATA)",
+	"STACK   (DATA)",
+	"CSEG    (CODE)",
+	"DSEG    (DATA)",
+	"ISEG    (DATA)",
+	"XSEG    (XDATA)",
+	"BSEG    (BIT)",
+	"RSEG    (DATA)",
+	"GSINIT  (CODE)",
+	"OSEG    (OVR,DATA)",
+	"GSFINAL (CODE)",
+	NULL,
+	NULL,
+	0,
+    },
+    { 
+	-1, 1, 4, 1, 1
+    },
+    /* avr has an 8 bit mul */
+    {
+	1
+    },
+    "_",
+    _avr_init,
+    _avr_parseOptions,
+    _avr_finaliseOptions,
+    _avr_setDefaultOptions,
+    avr_assignRegisters,
+    _avr_getRegName ,
+    _avr_keywords,
+    _avr_genAssemblerPreamble,
+    _avr_genIVT,
+    _avr_reset_regparm,
+    _avr_regparm,
+    FALSE
+};
+
---
+++ branches/gbdk-294/sdcc/src/mcs51/main.c
@@ -0,0 +1,254 @@
+/** @file main.c
+    mcs51 specific general functions.
+
+    Note that mlh prepended _mcs51_ on the static functions.  Makes
+    it easier to set a breakpoint using the debugger.
+*/
+#include "common.h"
+#include "main.h"
+#include "ralloc.h"
+#include "gen.h"
+
+static char _defaultRules[] =
+{
+#include "peeph.rul"
+};
+
+/* list of key words used by msc51 */
+static char *_mcs51_keywords[] =     {
+    "at",
+    "bit",
+    "code",
+    "critical",
+    "data",
+    "far",
+    "idata",
+    "interrupt",
+    "near",
+    "pdata",
+    "reentrant",
+    "sfr",
+    "sbit",
+    "using",
+    "xdata",
+    "_data",
+    "_code",
+    "_generic",
+    "_near",
+    "_xdata",
+    "_pdata",
+    "_idata",
+    NULL
+};
+
+
+void mcs51_assignRegisters (eBBlock **ebbs, int count);
+
+static int regParmFlg = 0; /* determine if we can register a parameter */
+
+static void _mcs51_init(void)
+{
+    asm_addTree(&asm_asxxxx_mapping);
+}
+
+static void _mcs51_reset_regparm()
+{
+    regParmFlg = 0;
+}
+
+static int _mcs51_regparm( link *l)
+{
+    /* for this processor it is simple
+       can pass only the first parameter in a register */
+    if (regParmFlg)
+	return 0;
+
+    regParmFlg = 1;
+    return 1;
+}
+
+static bool _mcs51_parseOptions(int *pargc, char **argv, int *i)
+{
+    /* TODO: allow port-specific command line options to specify
+     * segment names here.
+     */
+    return FALSE;
+}
+
+static void _mcs51_finaliseOptions(void)
+{
+    /* Hack-o-matic: if we are using the flat24 model,
+     * adjust pointer sizes.
+     */
+    if (options.model == MODEL_FLAT24)
+    {
+        port->s.fptr_size = 3;
+        port->s.gptr_size = 4;
+        port->stack.isr_overhead++;   /* Will save dpx on ISR entry. */
+        #if 1
+        port->stack.call_overhead++; 	   /* This acounts for the extra byte 
+        				    * of return addres on the stack.
+        				    * but is ugly. There must be a 
+        				    * better way.
+        				    */
+	#endif
+        fReturn = fReturn390;
+        fReturnSize = 5;
+    } 
+
+    if (options.model) {
+	port->mem.default_local_map = xdata;
+	port->mem.default_globl_map = xdata;
+    } else {
+	port->mem.default_local_map = data;
+	port->mem.default_globl_map = data;
+    }
+    
+    if (options.stack10bit)
+    {
+    	if (options.model != MODEL_FLAT24)
+    	{
+            fprintf(stderr, 
+        	    "*** warning: 10 bit stack mode is only supported in flat24 model.\n");
+            fprintf(stderr, "\t10 bit stack mode disabled.\n");
+            options.stack10bit = 0;
+        }
+        else
+        {
+            /* Fixup the memory map for the stack; it is now in
+             * far space and requires a FPOINTER to access it.
+             */
+            istack->fmap = 1;
+            istack->ptrType = FPOINTER; 
+        }
+    }
+}
+
+static void _mcs51_setDefaultOptions(void)
+{
+}
+
+static const char *_mcs51_getRegName(struct regs *reg)
+{
+    if (reg)
+	return reg->name;
+    return "err";
+}
+
+static void _mcs51_genAssemblerPreamble(FILE *of)
+{
+   if (options.model == MODEL_FLAT24)
+   {
+       fputs(".flat24 on\t\t; 24 bit flat addressing\n", of);
+       fputs("dpx = 0x93\t\t; dpx register unknown to assembler\n", of);
+       fputs("dps = 0x86\t\t; dps register unknown to assembler\n", of);
+       fputs("dpl1 = 0x84\t\t; dpl1 register unknown to assembler\n", of);
+       fputs("dph1 = 0x85\t\t; dph1 register unknown to assembler\n", of);
+       fputs("dpx1 = 0x95\t\t; dpx1 register unknown to assembler\n", of);
+   }
+}
+
+/* Generate interrupt vector table. */
+static int _mcs51_genIVT(FILE *of, symbol **interrupts, int maxInterrupts)
+{
+    int i;
+    
+    if (options.model != MODEL_FLAT24)
+    {
+        /* Let the default code handle it. */
+    	return FALSE;
+    }
+    
+    fprintf (of, "\tajmp\t__sdcc_gsinit_startup\n");
+    
+    /* now for the other interrupts */
+    for (i = 0; i < maxInterrupts; i++) 
+    {
+	if (interrupts[i])
+	{
+	    fprintf(of, "\tljmp\t%s\n\t.ds\t4\n", interrupts[i]->rname);
+	}
+	else
+	{
+	    fprintf(of, "\treti\n\t.ds\t7\n");
+	}
+    }
+    
+    return TRUE;
+}
+
+/** $1 is always the basename.
+    $2 is always the output file.
+    $3 varies
+    $l is the list of extra options that should be there somewhere...
+    MUST be terminated with a NULL.
+*/
+static const char *_linkCmd[] = {
+    "aslink", "-nf", "$1", NULL
+};
+
+static const char *_asmCmd[] = {
+    "asx8051", "-plosgffc", "$1.asm", NULL
+};
+
+/* Globals */
+PORT mcs51_port = {
+    "mcs51",
+    "MCU 8051",			/* Target name */
+    {
+	TRUE,			/* Emit glue around main */
+    },
+    {	
+	_asmCmd,
+	"-plosgffc",		/* Options with debug */
+	"-plosgff",		/* Options without debug */
+    },
+    {
+	_linkCmd
+    },
+    {
+	_defaultRules
+    },
+    {
+    	/* Sizes: char, short, int, long, ptr, fptr, gptr, bit, float, max */
+	1, 1, 2, 4, 1, 2, 3, 1, 4, 4
+    },
+    {
+	"XSEG    (XDATA)",
+	"STACK   (DATA)",
+	"CSEG    (CODE)",
+	"DSEG    (DATA)",
+	"ISEG    (DATA)",
+	"XSEG    (XDATA)",
+	"BSEG    (BIT)",
+	"RSEG    (DATA)",
+	"GSINIT  (CODE)",
+	"OSEG    (OVR,DATA)",
+	"GSFINAL (CODE)",
+	NULL,
+	NULL,
+	1
+    },
+    { 
+	+1, 1, 4, 1, 1
+    },
+    /* mcs51 has an 8 bit mul */
+    {
+	1
+    },
+    "_",
+    _mcs51_init,
+    _mcs51_parseOptions,
+    _mcs51_finaliseOptions,
+    _mcs51_setDefaultOptions,
+    mcs51_assignRegisters,
+    _mcs51_getRegName ,
+    _mcs51_keywords,
+    _mcs51_genAssemblerPreamble,
+    _mcs51_genIVT ,
+    _mcs51_reset_regparm,
+    _mcs51_regparm,
+    NULL,
+    FALSE
+};
+
