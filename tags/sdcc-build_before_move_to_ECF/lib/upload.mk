2af3b278f10a4f258f87f767ace710db3b55777a
bernhardheld <bernhardheld@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2007-02-24 13:10:40 +0000

    Last version for SF's CF before move to Erik's (Enhanced) CF

---
+++ tags/sdcc-build_before_move_to_ECF/lib/upload.mk
@@ -0,0 +1,25 @@
+# Makefile that uploades everything from $(HTDOCSDIR) to $(_WEBSNAPSHOTDEST)
+
+# Host and path used for removing old versions
+_WEBHOST=shell1.sourceforge.net
+_WEBHTDOCSDIR=/home/groups/s/sd/sdcc/htdocs
+
+_WEBSNAPSHOTDIR=$(_WEBHTDOCSDIR)/$(TARBALLBASE)
+_WEBREGTESTDIR=$(_WEBHTDOCSDIR)/$(REGTESTBASE)
+_WEBCHANGELOGDIR=$(_WEBHTDOCSDIR)/changelog_heads
+
+# Location to copy the tarball to
+_WEBSNAPSHOTDEST=$(_WEBHOST):$(_WEBHTDOCSDIR)
+
+.PHONY: do-upload remove-old-versions
+do-upload:
+	cd $(STAGINGBASE); \
+	rsync -r --include='*.exe' -e ssh --size-only $(HTDOCSDIR)/* $(_WEBSNAPSHOTDEST) && \
+	rm -rf $(HTDOCSDIR)/*
+
+# Removes old versions
+remove-old-versions:
+# over seven days old
+#	ssh $(_WEBHOST) 'cd $(_WEBSNAPSHOTDIR); find . -mtime +7 -not -type d -exec rm {} \;'
+# more than 7 files in dir
+	ssh $(_WEBHOST) 'find $(_WEBSNAPSHOTDIR)/* $(_WEBREGTESTDIR)/* $(_WEBCHANGELOGDIR) -type d -exec sh -c "cd {} ; ls -1t | sed 1,7d | xargs rm -f" \;'
