049a8df905f3b5693c076b1ddcc3543af2638b77
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-07-15 14:49:19 +0000

    Created stack compaction branch stack-compact

---
+++ branches/stack-compact/sdcc/support/regression/ports/ds390/spec.mk
@@ -0,0 +1,72 @@
+# Regression test specification for the ds390 target running with uCsim
+
+CC_FOR_BUILD = $(CC)
+
+# path to uCsim
+ifdef SDCC_BIN_PATH
+  S51 = $(SDCC_BIN_PATH)/s51$(EXEEXT)
+else
+  S51A = $(top_builddir)/sim/ucsim/s51.src/s51$(EXEEXT)
+  S51B = $(top_builddir)/bin/s51$(EXEEXT)
+
+  S51C = $(shell if [ -f $(S51A) ]; then echo $(S51A); else echo $(S51B); fi)
+
+ifndef CROSSCOMPILING
+  SDCCFLAGS += --nostdinc -I$(top_srcdir)
+  LINKFLAGS += --nostdlib -L$(top_builddir)/device/lib/build/ds390
+endif
+endif
+
+ifdef CROSSCOMPILING
+  DEV_NULL ?= NUL
+  SDCCFLAGS += -I$(top_srcdir)
+
+  EMU = wine $(S51C)
+else
+  DEV_NULL ?= /dev/null
+
+  EMU = $(S51C)
+endif
+
+SDCCFLAGS += -mds390 --less-pedantic -DREENTRANT=__reentrant -Wl-r
+LINKFLAGS += libsdcc.lib liblong.lib libint.lib libfloat.lib
+LINKFLAGS += libds390.lib
+
+OBJEXT = .rel
+BINEXT = .ihx
+
+# otherwise `make` deletes testfwk.rel and `make -j` will fail
+.PRECIOUS: $(PORT_CASES_DIR)/%$(OBJEXT)
+
+# Required extras
+EXTRAS = $(PORT_CASES_DIR)/testfwk$(OBJEXT) $(PORT_CASES_DIR)/support$(OBJEXT)
+include $(srcdir)/fwk/lib/spec.mk
+
+# Rule to link into .ihx
+%$(BINEXT): %$(OBJEXT) $(EXTRAS) $(FWKLIB) $(PORT_CASES_DIR)/fwk.lib
+	$(SDCC) $(SDCCFLAGS) $(LINKFLAGS) $(EXTRAS) $(PORT_CASES_DIR)/fwk.lib $< -o $@
+
+%$(OBJEXT): %.c
+	$(SDCC) $(SDCCFLAGS) -c $< -o $@
+
+$(PORT_CASES_DIR)/%$(OBJEXT): $(PORTS_DIR)/$(PORT)/%.c
+	$(SDCC) $(SDCCFLAGS) -c $< -o $@
+
+$(PORT_CASES_DIR)/%$(OBJEXT): $(srcdir)/fwk/lib/%.c
+	$(SDCC) $(SDCCFLAGS) -c $< -o $@
+
+$(PORT_CASES_DIR)/fwk.lib: $(srcdir)/fwk/lib/fwk.lib
+	cat < $(srcdir)/fwk/lib/fwk.lib > $@
+
+# run simulator with 25 seconds timeout
+%.out: %$(BINEXT) $(CASES_DIR)/timeout
+	mkdir -p $(dir $@)
+	-$(CASES_DIR)/timeout 25 $(EMU) -tds390 -S in=$(DEV_NULL),out=$@ $< < $(PORTS_DIR)/$(PORT)/uCsim.cmd > $(@:.out=.sim) \
+	  || echo -e --- FAIL: \"timeout, simulation killed\" in $(<:$(BINEXT)=.c)"\n"--- Summary: 1/1/1: timeout >> $@
+	python $(srcdir)/get_ticks.py < $(@:.out=.sim) >> $@
+	-grep -n FAIL $@ /dev/null || true
+
+$(CASES_DIR)/timeout: $(srcdir)/fwk/lib/timeout.c
+	$(CC_FOR_BUILD) $(CFLAGS) $< -o $@
+
+_clean:
---
+++ branches/td-newheur/sdcc/src/z80/z80.h
@@ -0,0 +1,47 @@
+/** @file z80/z80.h
+    Common definitions between the z80 and gbz80 parts.
+*/
+#include "common.h"
+#include "ralloc.h"
+#include "gen.h"
+#include "peep.h"
+#include "support.h"
+
+typedef enum
+  {
+    SUB_Z80,
+    SUB_GBZ80
+  }
+Z80_SUB_PORT;
+
+typedef struct
+  {
+    Z80_SUB_PORT sub;
+    int calleeSavesBC;
+    int port_mode;
+    int port_back;
+    int reserveIY;
+    int dump_graphs;
+    int optralloc_remat;
+    int oldralloc;
+  }
+Z80_OPTS;
+
+extern Z80_OPTS z80_opts;
+
+#define IS_GB  (z80_opts.sub == SUB_GBZ80)
+#define IS_Z80 (z80_opts.sub == SUB_Z80)
+#define IY_RESERVED (z80_opts.reserveIY)
+
+#define OPTRALLOC_REMAT (z80_opts.optralloc_remat)
+#define OPTRALLOC_EXACT_COST IS_Z80 // Todo: Implement exact cost for gbz80.
+#define OPTRALLOC_HL IS_Z80
+#define OPTRALLOC_A 1
+#define OPTRALLOC_IY !IY_RESERVED
+
+enum
+  {
+    ACCUSE_A = 1,
+    ACCUSE_SCRATCH,
+    ACCUSE_IY
+  };
