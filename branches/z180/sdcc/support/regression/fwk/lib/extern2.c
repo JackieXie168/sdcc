0c96054d4e3957a529412a57a6cb49fd95104273
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2011-11-17 14:11:23 +0000

    Cut z180 port feature branch, which has been merged to trunk.

--- branches/z180/sdcc/support/regression/fwk/lib/extern2.c
+++
@@ -1,9 +0,0 @@
-/* needed by tests/inline.c */
-
-#ifdef SDCC
-#pragma std_sdcc99
-#endif
-
-extern char inlined_function (void);
-
-extern char (*inlined_function_pointer) (void) = &inlined_function;
