27c731b4f906f7849065b89cbc72ffd9ddc423f7
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-05-10 12:07:23 +0000

    Cut obsolete branch.

--- branches/gbdk-297/sdcc/device/lib/_slong2fs.c
+++
@@ -1,9 +0,0 @@
-#include <float.h>
-
-/* convert signed long to float */
-float __slong2fs (signed long sl) {
-  if (sl<0) 
-    return -__ulong2fs(-sl);
-  else 
-    return __ulong2fs(sl);
-}
--- branches/gbdk-297/sdcc/device/lib/_uint2fs.c
+++
@@ -1,6 +0,0 @@
-#include <float.h>
-
-/* convert unsigned int to float */
-float __uint2fs (unsigned int ui) {
-  return __ulong2fs(ui);
-}
