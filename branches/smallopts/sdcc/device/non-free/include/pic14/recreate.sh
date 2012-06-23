4359011d18a191a783f8345c53745f386fcae6c0
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-06-23 15:29:47 +0000

    Merge from trunk.

--- branches/smallopts/sdcc/device/non-free/include/pic14/recreate.sh
+++ branches/smallopts/sdcc/device/non-free/include/pic14/recreate.sh
@@ -10,11 +10,45 @@
 GPUTILS=$HOME/svn/gputils
 SDCC=$HOME/svn/plain
 
-for i in ../pic*.h; do
-    if [ ! "$i" = "../pic14regs.h" ]; then
-	DEV=`echo "$i" | sed -e "s:../pic::;s/\.h//"`;
-	echo "Creating ${DEV}...";
-	"${SDCC}/support/scripts/inc2h.pl" "${DEV}" "${GPUTILS}";
+is_in()
+{
+  local f l j
+  f=$1
+  shift
+  l=$*
+
+  for j in $l; do
+    if [ $f = $j ]
+    then
+      return 0
     fi
+  done
+
+  return 1
+}
+
+NO_LEGACY_NAMES=
+
+for i in ../pic*.h
+do
+  if ! is_in $i ../pic14regs.h ../pic16fam.h ]
+  then
+    test -e $i && grep -q NO_LEGACY_NAMES $i && NO_LEGACY_NAMES="$NO_LEGACY_NAMES $i"
+  fi
 done
 
+for i in ../pic*.h
+do
+  if ! is_in $i ../pic14regs.h ../pic16fam.h ]
+  then
+    if is_in $i $NO_LEGACY_NAMES
+    then
+      emit_legacy_names=1
+    else
+      emit_legacy_names=
+    fi
+    DEV=`echo "$i" | sed -e "s:../pic::;s/\.h//"`;
+    echo "Creating ${DEV} ${emit_legacy_names}...";
+    "${SDCC}/support/scripts/inc2h.pl" "${DEV}" "${GPUTILS}" "${emit_legacy_names}";
+  fi
+done
