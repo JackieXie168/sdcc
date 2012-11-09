07f6d68af9e381eb343f764d1629c824292e3a84
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-11-09 12:39:42 +0000

    update of copyright info

--- branches/ucsim/ucsim-0_6-pre/appcl.h
+++ branches/ucsim/ucsim-0_6-pre/appcl.h
@@ -1,7 +1,7 @@
 /*
  * Simulator of microcontrollers (appcl.h)
  *
- * Copyright (C) 2001,01 Drotos Daniel, Talker Bt.
+ * Copyright (C) 1997,12 Drotos Daniel, Talker Bt.
  * 
  * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
  *
