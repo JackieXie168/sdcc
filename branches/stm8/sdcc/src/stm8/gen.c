228c0c983e951e1b9b0751126f971dfd18df1065
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2012-12-28 11:46:37 +0000

    Infrastructure for STM8 port.

--- branches/stm8/sdcc/src/stm8/gen.c
+++ branches/stm8/sdcc/src/stm8/gen.c
@@ -2,6 +2,7 @@
   gen.c - code generator for STM8.
 
   Copyright (C) 2012, Philipp Klaus Krause pkk@spth.de, philipp@informatik.uni-frankfurt.de)
+                2011, Vaclav Peroutka
 
   This program is free software; you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by the
@@ -22,13 +23,28 @@
 #include "gen.h"
 
 static bool regalloc_dry_run;
-static unsigned char regalloc_dry_run_cost;
+static unsigned char regalloc_dry_run_cost_bytes;
+static unsigned char regalloc_dry_run_cost_cycles;
 
 static struct
 {
   short debugLine;
 }
 _G;
+
+static void cost(unsigned int bytes, unsigned int cycles)
+{
+  regalloc_dry_run_cost_bytes += bytes;
+  regalloc_dry_run_cost_cycles += cycles;
+}
+
+/*--------------------------------------------------------------------------*/
+/* adjustStack - Adjust the stack pointer by n bytes.                       */
+/*--------------------------------------------------------------------------*/
+static void
+adjustStack (int n)
+{
+}
 
 /*---------------------------------------------------------------------*/
 /* stm8_emitDebuggerSymbol - associate the current code location       */
@@ -42,6 +58,88 @@ stm8_emitDebuggerSymbol (const char *debugSym)
   _G.debugLine = 0;
 }
 
+/*-----------------------------------------------------------------*/
+/* genFunction - generated code for function entry                 */
+/*-----------------------------------------------------------------*/
+static void
+genFunction (iCode *ic)
+{
+  const symbol *sym = OP_SYMBOL_CONST (IC_LEFT (ic));
+  sym_link *ftype = operandType (IC_LEFT (ic));
+
+  /* create the function header */
+  emitcode (";", "-----------------------------------------");
+  emitcode (";", " function %s", sym->name);
+  emitcode (";", "-----------------------------------------");
+
+  emitcode ("", "%s:", sym->rname);
+  genLine.lineCurr->isLabel = 1;
+
+  if (IFFUNC_ISNAKED(ftype))
+  {
+      emitcode(";", "naked function: no prologue.");
+      return;
+  }
+
+  /* adjust the stack for the function */
+  if (sym->stack)
+    adjustStack (-sym->stack);
+}
+
+/*-----------------------------------------------------------------*/
+/* genEndFunction - generates epilogue for functions               */
+/*-----------------------------------------------------------------*/
+static void
+genEndFunction ( iCode *ic)
+{
+  symbol *sym = OP_SYMBOL (IC_LEFT (ic));
+
+  if (IFFUNC_ISNAKED(sym->type))
+  {
+      emitcode(";", "naked function: no epilogue.");
+      if (options.debug && currFunc)
+        debugFile->writeEndFunction (currFunc, ic, 0);
+      return;
+  }
+
+  if (sym->stack)
+    adjustStack (sym->stack);
+
+  if (IFFUNC_ISISR (sym->type))
+    {
+      /* if debug then send end of function */
+      if (options.debug && currFunc)
+        {
+          debugFile->writeEndFunction (currFunc, ic, 1);
+        }
+
+      emitcode ("iret", "");
+      cost (1, 11);
+    }
+  else
+    {
+      /* if debug then send end of function */
+      if (options.debug && currFunc)
+        debugFile->writeEndFunction (currFunc, ic, 1);
+
+      emitcode ("ret", "");
+      cost (1, 11);
+    }
+}
+
+/*-----------------------------------------------------------------*/
+/* genLabel - generates a label                                    */
+/*-----------------------------------------------------------------*/
+static void
+genLabel (const iCode * ic)
+{
+  /* special case never generate */
+  if (IC_LABEL (ic) == entryLabel)
+    return;
+
+  emitLabel (IC_LABEL (ic));
+}
+
 /*---------------------------------------------------------------------*/
 /* genSTM8Code - generate code for STM8 for a single iCode instruction */
 /*---------------------------------------------------------------------*/
@@ -50,6 +148,79 @@ genSTM8iCode (iCode *ic)
 {
   switch (ic->op)
     {
+    case '!':
+    case '~':
+    case UNARYMINUS:
+    case IPUSH:
+    case IPOP:
+    case CALL:
+    case PCALL:
+      wassertl (0, "Unimplemented iCode");
+      break;
+
+    case FUNCTION:
+      genFunction (ic);
+      break;
+
+    case ENDFUNCTION:
+      genEndFunction (ic);
+      break;
+
+    case RETURN:
+      wassertl (0, "Unimplemented iCode");
+      break;
+
+    case LABEL:
+      genLabel (ic);
+      break;
+
+    case GOTO:
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+    case '>':
+    case '<':
+      wassertl (0, "Unimplemented iCode");
+      break;
+
+    case LE_OP:
+    case GE_OP:
+    case NE_OP:
+
+      /* note these two are xlated by algebraic equivalence
+         during parsing SDCC.y */
+      werror (E_INTERNAL_ERROR, __FILE__, __LINE__, "got '>=' or '<=' shouldn't have come here");
+      break;
+
+    case EQ_OP:
+    case AND_OP:
+    case OR_OP:
+    case '^':
+    case '|':
+    case BITWISEAND:
+    case INLINEASM:
+    case RRC:
+    case RLC:
+    case GETHBIT:
+    case GETABIT:
+    case LEFT_OP:
+    case RIGHT_OP:
+    case GET_VALUE_AT_ADDRESS:
+    case '=':
+    case IFX:
+    case ADDRESS_OF:
+    case JUMPTABLE:
+    case CAST:
+    case RECEIVE:
+    case SEND:
+    case DUMMY_READ_VOLATILE:
+    case CRITICAL:
+    case ENDCRITICAL:
+      wassertl (0, "Unimplemented iCode");
+      break;
+
     default:
       wassertl (0, "Unknown iCode");
     }
@@ -59,7 +230,8 @@ unsigned char
 drySTM8iCode (iCode *ic)
 {
   regalloc_dry_run = TRUE;
-  regalloc_dry_run_cost = 0;
+  regalloc_dry_run_cost_bytes = 0;
+  regalloc_dry_run_cost_cycles = 0;
 
   initGenLineElement ();
 
@@ -67,7 +239,7 @@ drySTM8iCode (iCode *ic)
 
   destroy_line_list ();
 
-  return (regalloc_dry_run_cost);
+  return (regalloc_dry_run_cost_bytes);
 }
 
 #ifdef DEBUG_DRY_COST
@@ -93,14 +265,14 @@ genSTM8Code (iCode *lic)
 #endif
 
   iCode *ic;
-  int cln = 0;
   regalloc_dry_run = FALSE;
 
   for (ic = lic; ic; ic = ic->next)
     {
       initGenLineElement ();
 
-      regalloc_dry_run_cost = 0;
+      regalloc_dry_run_cost_bytes = 0;
+      regalloc_dry_run_cost_cycles = 0;
 
       genSTM8iCode(ic);
     }
