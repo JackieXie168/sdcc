d017005bc3cc3eb4fc44380909d4e62ac4080770
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2013-06-29 18:21:43 +0000

    Preparations for better handling of register-pressure in lospre.

--- sdcc/src/SDCCBBlock.c
+++ sdcc/src/SDCCBBlock.c
@@ -313,6 +313,7 @@ iCode2eBBlock (iCode * ic)
 
   /* put the first one unconditionally */
   ebb->sch = ic;
+  ic->seq = 0;
 
   /* if this is a label then */
   if (ic->op == LABEL)
@@ -359,6 +360,7 @@ iCode2eBBlock (iCode * ic)
   /* loop thru till we find one with a label */
   for (loop = ic->next; loop; loop = loop->next)
     {
+      loop->seq = 0;
 
       /* if this is the last one */
       if (!loop->next)
